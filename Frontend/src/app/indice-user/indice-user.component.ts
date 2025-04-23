a3ce1d12aaa1dcf8bb8f0408de45b54cf5e25351
FelipeAT <pipeaviles1608@gmail.com>
2025-04-22 21:56:28 -0500

    Listando User

--- Frontend/src/app/indice-user/indice-user.component.ts
+++ Frontend/src/app/indice-user/indice-user.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-indice-user',
-  imports: [],
+  standalone: true,
+  imports: [MatCardModule, MatTableModule, MatIconModule, MatButtonModule, CommonModule],
   templateUrl: './indice-user.component.html',
   styleUrl: './indice-user.component.css'
 })
-export class IndiceUserComponent {
+export class IndiceUserComponent implements OnInit {
+  private userService = inject(UserService);
+  usuarios: any[] = [];
+  columnas = ['id', 'username', 'password', 'personName', 'acciones'];
 
+  ngOnInit(): void {
+    this.cargarUsuarios();
+  }
+
+
+  cargarUsuarios(): void {
+    this.userService.getAll().subscribe({
+      next: data => this.usuarios = data,
+      error: err => console.error("Error cargando usuarios", err)
+    });
+  }
+
+  editar(id: number): void {
+    console.log("Editar", id);
+    // lógica futura: redirigir al formulario o abrir modal
+  }
+
+  eliminar(id: number): void {
+    if (confirm("¿Eliminar este usuario?")) {
+      this.userService.delete(id).subscribe({
+        next: () => this.cargarUsuarios(),
+        error: err => console.error("Error eliminando", err)
+      });
+    }
+  }
 }
---
+++ Frontend/src/app/services/user.service.ts
@@ -0,0 +1,32 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private http = inject(HttpClient);
+  private baseUrl = `${environment.apiURL}api/User/`;
+
+  getAll(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}GetAll/`);
+  }
+
+  getById(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}GetByiId/${id}/`);
+  }
+
+  create(user: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}Create/`, user);
+  }
+
+  update(user: any): Observable<any> {
+    return this.http.put(`${this.baseUrl}Update/`, user);
+  }
+
+  delete(id: number, strategy = 0): Observable<any> {
+    return this.http.delete(`${this.baseUrl}Delete/${id}/?strategy=${strategy}`);
+  }
+}
